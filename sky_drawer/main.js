696ca762ec074d5f966c65c0d134286403978915
Jamie Stevens <ste616@gmail.com>
2017-05-02 22:17:28 +1000

    Codebase as is, but far out it takes a long time to load.

--- sky_drawer/main.js
+++ sky_drawer/main.js
@@ -1,2 +1,29 @@
-// Routines for the Sky Drawer.
+// Routines for the Sky Drawer main page.
+require([
+  "dojo/dom",
+  "dojo/domReady!"
+], function(dom) {
+  // The WWT object.
+  var wwt;
 
+  // Standard wat to initialise the WorldWide Telescope.
+  var initForWwt = function() {
+    wwt.loadImageCollection("http://www.worldwidetelescope.org/COMPLETE/wwtcomplete.wtml");
+    wwt.settings.set_showCrosshairs(true);
+    wwt.hideUI(false);
+  };
+  
+  // Function called by the WWT initialiser when it's ready.
+  var wwtReady = function() {
+    initForWwt();
+  };
+  
+  // Function to initialise the WWT object.
+  var wwtInit = function() {
+    wwt = wwtlib.WWTControl.initControl("WWTCanvas");
+    wwt.add_ready(wwtReady);
+  };
+
+  // Call it.
+  wwtInit();
+});
